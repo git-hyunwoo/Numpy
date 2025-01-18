34f48433925042f8f356a80e834dfccaae2fe96f
dev choi <choiman2021@gmail.com>
2025-01-18 12:43:35 +0900

    numpy list structure

--- Chapter 01/example_01_structure_of_list.py
+++ Chapter 01/example_01_structure_of_list.py
@@ -1,4 +1,11 @@
 import numpy as np
 
+print(f"np.__version__ : {np.__version__}")
+
 l = [[1,2,3], [4,5,6]]
 print(f"type(l) : {type(l)}")
+
+for i in dir(np.ndarray):
+    if not i.startswith("__"):
+        if type(np.ndarray.__dict__[i]) != type(np.ndarray.var):
+            print(i)
