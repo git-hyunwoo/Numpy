92a636ae1b9f71584d6892cb6ec07667f623c408
dev choi <choiman2021@gmail.com>
2025-01-18 18:16:06 +0900

    example 2-1

---
+++ Chapter 01/example_02_creating_list.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# 기본 리스트 선언
+l = [1,2,3,4]
+
+# convert to numpy array
+a = np.array(l)
+
+print(a)
+print(f"type(a) : {type(a)}")
+
+# python tuple
+t = (1,2,3,4)
+
+# convert to numpy array
+b = np.array(t)
+
+print(b)
+print(f"type(b) : {type(b)}")
