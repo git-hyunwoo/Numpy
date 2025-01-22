deb84469bd9ab4c23dc752fb6ca6b114c2ea6c29
dev choi <choiman2021@gmail.com>
2025-01-22 23:27:13 +0900

    new element

--- Chapter 01/example_02_creating_list.py
+++ Chapter 01/example_02_creating_list.py
@@ -27,3 +27,8 @@ print(f"a.data.obj : {a.data.obj}")
 # 속성 base는 다차원 배열의 메모리를 공유할 때 원본 레퍼런스를 저장한다.
 c = b
 print(f"c.base is b.base : {c.base is b.base}")
+
+# 새로운 변수 c에 저장된 첫 번째 원소를 변경하면 내부에 저장된 원본 배열을 변경하다.
+# 모든 변수를 조회하면 다차원 배열의 변경된 것을 볼 수 있다.
+c[0] = 100
+print(f"c : {c}")
